9822cd7100d2c8795095df08e325ce271805bf82
RiyadKhalifeh <riyadkhalifehit@gmail.com>
2019-04-30 13:38:27 +0300

    'V1'

---
+++ خوارزمية بلايفير/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace خوارزمية_بلايفير
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridViewKey = new System.Windows.Forms.DataGridView();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxPlanText = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxCipherText = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBoxDecipherText = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKey)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1229, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridViewKey);
+            this.groupBox1.Controls.Add(this.textBoxKey);
+            this.groupBox1.Location = new System.Drawing.Point(12, 86);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox1.Size = new System.Drawing.Size(652, 308);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "جدول التشفير";
+            // 
+            // dataGridViewKey
+            // 
+            this.dataGridViewKey.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewKey.BackgroundColor = System.Drawing.Color.LightGray;
+            this.dataGridViewKey.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewKey.Location = new System.Drawing.Point(29, 53);
+            this.dataGridViewKey.Name = "dataGridViewKey";
+            this.dataGridViewKey.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.dataGridViewKey.RowTemplate.Height = 26;
+            this.dataGridViewKey.Size = new System.Drawing.Size(586, 237);
+            this.dataGridViewKey.TabIndex = 1;
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(29, 23);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBoxKey.Size = new System.Drawing.Size(586, 24);
+            this.textBoxKey.TabIndex = 0;
+            this.textBoxKey.TextChanged += new System.EventHandler(this.textBoxKey_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBoxPlanText);
+            this.groupBox2.Location = new System.Drawing.Point(700, 86);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox2.Size = new System.Drawing.Size(489, 93);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "النص الأصلي";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(402, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "تشفير";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxPlanText
+            // 
+            this.textBoxPlanText.Location = new System.Drawing.Point(29, 23);
+            this.textBoxPlanText.Multiline = true;
+            this.textBoxPlanText.Name = "textBoxPlanText";
+            this.textBoxPlanText.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBoxPlanText.Size = new System.Drawing.Size(367, 52);
+            this.textBoxPlanText.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.textBoxCipherText);
+            this.groupBox3.Location = new System.Drawing.Point(700, 196);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox3.Size = new System.Drawing.Size(489, 93);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "النص المشفر";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(402, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "فك تشفير";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxCipherText
+            // 
+            this.textBoxCipherText.Location = new System.Drawing.Point(27, 23);
+            this.textBoxCipherText.Multiline = true;
+            this.textBoxCipherText.Name = "textBoxCipherText";
+            this.textBoxCipherText.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBoxCipherText.Size = new System.Drawing.Size(369, 52);
+            this.textBoxCipherText.TabIndex = 2;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Controls.Add(this.textBoxDecipherText);
+            this.groupBox4.Location = new System.Drawing.Point(700, 301);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.groupBox4.Size = new System.Drawing.Size(489, 93);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "النص مفكوك التشفير";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(408, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "مسح";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBoxDecipherText
+            // 
+            this.textBoxDecipherText.Location = new System.Drawing.Point(27, 23);
+            this.textBoxDecipherText.Multiline = true;
+            this.textBoxDecipherText.Name = "textBoxDecipherText";
+            this.textBoxDecipherText.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBoxDecipherText.Size = new System.Drawing.Size(369, 52);
+            this.textBoxDecipherText.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(617, 400);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 30);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "تشفير";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1229, 466);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "خوارزمية بلايفير";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewKey)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.DataGridView dataGridViewKey;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxPlanText;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxCipherText;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxDecipherText;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ خوارزمية بلايفير/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Bluebit.MatrixLibrary;
+
+
+namespace خوارزمية_بلايفير
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //string alphabets = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+        string alphabets = "أبتثجحخدذرزسشصضطظعغفقكلمنهوي";
+
+
+        private void button3_Click(object sender, EventArgs e)   ///مسح مربعات النصوص 
+        {
+            textBoxKey.Clear();
+            textBoxPlanText.Clear();
+            textBoxCipherText.Clear();
+            textBoxDecipherText.Clear();
+        }
+
+        private void textBoxKey_TextChanged(object sender, EventArgs e)  ///بناء جدول التشفير
+        {
+            if (textBoxKey.Text == "")
+            {
+                dataGridViewKey.DataSource = null;
+            }
+            else
+            {
+                string key = textBoxKey.Text.ToUpper();
+                //key = key.Replace('J', 'I');                     // استبدال كل جي بـ آي
+
+                for (int i = 0; i < key.Count(); i++)           // إزالة الأحرف المكررة من جدول التشفير
+                {
+                    for (int j = i + 1; j < key.Count(); j++)
+                    {
+                        if (key[i] == key[j])
+                        {
+                            key = key.Remove(j, 1);
+                            j--;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < 28 /*25*/; i++)                    // تعبئة حروف جدول التشفير بسترينج
+                {
+                    if (key.Contains(alphabets[i]))             //A=>Z
+                        continue;
+                    else
+                        key = key + alphabets[i];               //TashferBCD
+                }
+
+                List<string[]> list = new List<string[]>();
+                for (int i = 0; i < 28/*25*/; i += 4)
+                {
+                    list.Add(new string[] { Convert.ToString(key[i]), Convert.ToString(key[i + 1]), Convert.ToString(key[i + 2]), Convert.ToString(key[i + 3]), /*Convert.ToString(key[i + 4])*/ });
+                }
+
+                DataTable table = ConvertListToDataTable(list);
+                dataGridViewKey.DataSource = table;
+                //dataGridViewKey.DataSource = from table12 in list
+                //                             select table12;
+            }
+        }
+
+        static DataTable ConvertListToDataTable(List<string[]> list)
+        {
+            // New table.
+            DataTable table = new DataTable();
+
+            // Get max columns.
+            int columns = 0;
+            foreach (var array in list)
+            {
+                if (array.Length > columns)
+                {
+                    columns = array.Length;
+                }
+            }
+
+            // Add columns.
+            for (int i = 0; i < columns; i++)
+            {
+                table.Columns.Add();
+            }
+
+            // Add rows.
+            foreach (var array in list)
+            {
+                table.Rows.Add(array);
+            }
+
+            return table;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////
+
+        public static int Mod(int a, int n)
+        {
+            return a - (int)Math.Floor((double)a / n) * n;
+        }
+
+        private void button1_Click(object sender, EventArgs e)  /// تشفير النص الأصلي
+        {
+            if (textBoxKey.Text == "" || textBoxPlanText.Text == "")
+                MessageBox.Show("بعض الحقول المطلوبة فارغة");
+            else
+            {
+                textBoxCipherText.Clear();
+                string PlanText = textBoxPlanText.Text.ToUpper() + " ";
+                //PlanText = PlanText.Replace("J", "I");
+                for (int i = 0; i < PlanText.Length; i += 2)
+                {
+                    if (alphabets.Contains(PlanText[i]))
+                    {
+                        if (!alphabets.Contains(PlanText[i + 1]))
+                        {
+                            if (PlanText[i] == 'س')
+                                PlanText = PlanText.Insert(i + 1, "ص");
+                            else
+                                PlanText = PlanText.Insert(i + 1, "س");
+                        }
+
+                        if (PlanText[i] == PlanText[i + 1])
+                        {
+                            if (PlanText[i] == 'س')
+                            {
+                                PlanText = PlanText.Insert(i + 1, "ص");
+                            }
+                            else
+                                PlanText = PlanText.Insert(i + 1, "س");
+                        }
+                        
+                        
+                        int RowChar1 = 0;
+                        int ColumnChar1 = 0;
+                        int RowChar2 = 0;
+                        int ColumnChar2 = 0;
+
+                        for (int R = 0; R < 7 /*5*/; R++)
+                        {
+                            for (int C = 0; C < 4 /*5*/; C++)
+                            {
+
+                                if (dataGridViewKey.Rows[R].Cells[C].Value.ToString() == Convert.ToString(PlanText[i]))
+                                {
+                                    RowChar1 = R;
+                                    ColumnChar1 = C;
+                                }
+                                if (dataGridViewKey.Rows[R].Cells[C].Value.ToString() == Convert.ToString(PlanText[i + 1]))
+                                {
+                                    RowChar2 = R;
+                                    ColumnChar2 = C;
+                                }
+
+                            }
+                        }
+                        if (RowChar1 == RowChar2)
+                        {
+                            textBoxCipherText.Text += dataGridViewKey.Rows[RowChar1].Cells[Mod(ColumnChar1 + 1, 4/*5*/)].Value.ToString();
+                            textBoxCipherText.Text += dataGridViewKey.Rows[RowChar1].Cells[Mod(ColumnChar2 + 1, 4/*5*/)].Value.ToString();
+                        }
+                        else
+                            if (ColumnChar1 == ColumnChar2)
+                            {
+                                textBoxCipherText.Text += dataGridViewKey.Rows[Mod(RowChar1 + 1, 7/*5*/)].Cells[ColumnChar1].Value.ToString();
+                                textBoxCipherText.Text += dataGridViewKey.Rows[Mod(RowChar2 + 1, 7/*5*/)].Cells[ColumnChar1].Value.ToString();
+                            }
+                            else
+                            {
+                                textBoxCipherText.Text += dataGridViewKey.Rows[RowChar1].Cells[ColumnChar2].Value.ToString();
+                                textBoxCipherText.Text += dataGridViewKey.Rows[RowChar2].Cells[ColumnChar1].Value.ToString();
+                            }
+                    }
+                    else
+                    {
+                        textBoxCipherText.Text += PlanText[i];
+                        i--;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)      /// عملية فك التشفير
+        {
+            if (textBoxKey.Text == "" || textBoxCipherText.Text == "")
+                MessageBox.Show("بعض الحقول المطلوبة فارغة");
+            else
+            {
+                textBoxDecipherText.Clear();
+                string CipherText = textBoxCipherText.Text.ToUpper() + " ";
+                for (int i = 0; i < CipherText.Length; i += 2)
+                {
+                    if (alphabets.Contains(CipherText[i]))
+                    {
+                        int RowChar1 = 0;
+                        int ColumnChar1 = 0;
+                        int RowChar2 = 0;
+                        int ColumnChar2 = 0;
+
+                        for (int R = 0; R < 7; R++)
+                        {
+                            for (int C = 0; C < 4; C++)
+                            {
+
+                                if (dataGridViewKey.Rows[R].Cells[C].Value.ToString() == Convert.ToString(CipherText[i]))
+                                {
+                                    RowChar1 = R;
+                                    ColumnChar1 = C;
+                                }
+                                if (dataGridViewKey.Rows[R].Cells[C].Value.ToString() == Convert.ToString(CipherText[i + 1]))
+                                {
+                                    RowChar2 = R;
+                                    ColumnChar2 = C;
+                                }
+
+                            }
+                        }
+                        if (RowChar1 == RowChar2)
+                        {
+                            textBoxDecipherText.Text += dataGridViewKey.Rows[RowChar1].Cells[Mod(ColumnChar1 - 1, 4/*5*/)].Value.ToString();
+                            textBoxDecipherText.Text += dataGridViewKey.Rows[RowChar1].Cells[Mod(ColumnChar2 - 1, 4/*5*/)].Value.ToString();
+                        }
+                        else
+                            if (ColumnChar1 == ColumnChar2)
+                            {
+                                textBoxDecipherText.Text += dataGridViewKey.Rows[Mod(RowChar1 - 1, 7/*5*/)].Cells[ColumnChar1].Value.ToString();
+                                textBoxDecipherText.Text += dataGridViewKey.Rows[Mod(RowChar2 - 1, 7/*5*/)].Cells[ColumnChar1].Value.ToString();
+                            }
+                            else
+                            {
+                                textBoxDecipherText.Text += dataGridViewKey.Rows[RowChar1].Cells[ColumnChar2].Value.ToString();
+                                textBoxDecipherText.Text += dataGridViewKey.Rows[RowChar2].Cells[ColumnChar1].Value.ToString();
+                            }
+                    }
+                    else
+                    {
+                        textBoxDecipherText.Text += CipherText[i];
+                        i--;
+                    }
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bluebit.MatrixLibrary.Matrix a = new Bluebit.MatrixLibrary.Matrix(3, 3);
+            Bluebit.MatrixLibrary.Matrix A = new Bluebit.MatrixLibrary.Matrix(new double[,] { { 3, 0 }, { 2, 1 } });
+            //a = A.Inverse();
+            MessageBox.Show(A.Determinant().ToString());
+            //textBoxKey.Text = a[0, 0].ToString() + "  " + a[0, 1].ToString() + "  " + a[1, 0].ToString()+"  " + a[0, 1].ToString();
+            dataGridViewKey.DataSource = a.ToArray(); 
+
+
+        }
+    }
+}
